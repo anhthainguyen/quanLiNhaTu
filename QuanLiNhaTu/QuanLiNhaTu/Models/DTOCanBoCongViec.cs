84f5781953f7dbba46e50109ae9543767014ebb1
ThaiQuangKhai <vnthai084@gmail.com>
2019-05-30 09:01:48 +0700

    cái phần của quản lý

---
+++ QuanLiNhaTu/QuanLiNhaTu/Models/DTOCanBoCongViec.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiNhaTu.Models
+{
+    public class DTOCanBoCongViec
+    {
+        public string Ma { get; set; }
+        public string Ten_CongViec { get; set; }
+        public string Ten_CanBo { get; set; }
+    }
+}
---
+++ QuanLiNhaTu/QuanLiNhaTu/ViewModels/MainQuanLyViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using QuanLiNhaTu.Views;
+using QuanLiNhaTu.Models;
+
+namespace QuanLiNhaTu.ViewModels
+{
+    public class MainQuanLyViewModel : BaseViewModel
+    {
+        public ICommand ThanNhanCommand { get; set; }
+        public ICommand TuNhanCommand { get; set; }
+        public ICommand CanBoCommand { get; set; }
+        public MainQuanLyViewModel()
+        {
+            ThanNhanCommand = new RelayCommand<object>((p) => { return true; }, (p) => { ThanNhan wd = new ThanNhan(); wd.ShowDialog(); });
+            TuNhanCommand = new RelayCommand<object>((p) => { return true; }, (p) => { TuNhan wd = new TuNhan(); wd.ShowDialog(); });
+            CanBoCommand = new RelayCommand<object>((p) => { return true; }, (p) => { ChiaLichTruc wd = new ChiaLichTruc(); wd.ShowDialog(); });
+        }
+    }
+}
